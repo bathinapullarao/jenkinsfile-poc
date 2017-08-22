9e04cc37480e8e5b29b424789174f95c3136f104
chinthakadd <chinthakadd@gmail.com>
2017-08-22 00:10:11 -0400

    will jenkins kickin

--- src/main/groovy/com/techcoaster/pipeline/jenkinsfilepoc/HelloWorldController.groovy
+++ src/main/groovy/com/techcoaster/pipeline/jenkinsfilepoc/HelloWorldController.groovy
@@ -1,11 +1,13 @@
 package com.techcoaster.pipeline.jenkinsfilepoc
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
 class HelloWorldController {
 
+    @GetMapping
     String hello() {
         return "Hello World TEST"
     }
